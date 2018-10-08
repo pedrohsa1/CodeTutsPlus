50f741e7091e75f327182ce96bc5368a8ed15ec9
Pedro HSA <phenriquesa94@gmail.com>
2018-10-08 15:58:09 -0300

    Primeiro commit Code Tuts Plus

---
+++ basic/views/site/say.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PedroHSA
+ * Date: 08/10/2018
+ * Time: 14:39
+ */
+use yii\helpers\Html;
+?>
+
+<h1>Hello <?= Html::encode($target) ?> </h1>
+<p>Welcome to your Yii2 demonstration application.</p>
+
+
